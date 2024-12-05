3342186a362642fe7bfd9f87bbca282691d135d4
RVNJHN08 <ajsuyat8@gmail.com>
2024-12-05 14:38:37 +0800

    Initial commit

---
+++ Day2Card/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card.jsx";
+import Card1 from "./Card1.jsx";
+function App() {
+  return (
+    <>
+      <Card></Card>
+      <Card1></Card1>
+    </>
+  );
+}
+
+export default App;
